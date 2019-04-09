08ad2d2d560268cccf297e9fe06a465ac515307e
davichodavid <davichodavid@yahoo.com>
2019-04-09 17:41:08 -0400

    Added a few more

--- js/index.js
+++ js/index.js
@@ -5,9 +5,41 @@ busPic.addEventListener("click", event => {
   event.target.classList.toggle("bigSmallBus");
 });
 
-/***********************  MAP IMAGE **************************************** */
+/***********************  INVERSE CONTENT **************************************** */
 const inverseContent = document.querySelector("#inverse-content");
 inverseContent.addEventListener("dblclick", event => {
   event.currentTarget.classList.toggle("inverse-content");
-  console.log(event.target);
+});
+
+/***********************  FLIP CONTENT **************************************** */
+const flipMap = document.querySelectorAll(".img-content")[0];
+
+const keyIn = document.addEventListener("keypress", event => {
+  flipMap.style.transform = "rotateY(180deg)";
+  flipMap.style.transformStyle = "preserve-3d";
+  flipMap.style.transition = "all 1.0s linear";
+});
+
+/***********************  H2 TEXT CONTENT **************************************** */
+
+const headerH2 = document.querySelector("header h2");
+
+headerH2.addEventListener("mouseover", event => {
+  event.target.textContent = "Welcome To The Jungle";
+});
+
+/***********************  FlIPPED AGAIN TEXT CONTENT **************************************** */
+const flipItAgain = document.querySelector("#secondImg");
+
+const keyOut = document.addEventListener("scroll", event => {
+  flipItAgain.style.transform = "rotateX(-360deg)";
+  flipItAgain.style.transformStyle = "preserve-3d";
+  flipItAgain.style.transition = "all 2.0s linear";
+});
+
+/*********************** CUT H2  **************************************** */
+const reverseContent = document.querySelector("#inverse-content h2");
+
+reverseContent.addEventListener("copy", event => {
+  event.target.textContent = "Hey, are you taking something from my page?";
 });
