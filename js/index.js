823cea7151632e69546cea7e59940bcc649ce748
davichodavid <davichodavid@yahoo.com>
2019-04-09 15:59:54 -0400

    Used classList toggle to add and remove existing and added classes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+/***********************  BUS IMAGE **************************************** */
+const busPic = document.querySelector("header img");
+busPic.addEventListener("click", event => {
+  event.target.classList.toggle("bigSmallBus");
+});
+
+/***********************  MAP IMAGE **************************************** */
+const inverseContent = document.querySelector("#inverse-content");
+inverseContent.addEventListener("dblclick", event => {
+  event.currentTarget.classList.toggle("inverse-content");
+  console.log(event.target);
+});
